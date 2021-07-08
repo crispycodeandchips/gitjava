4ad6776aa86e688d7b30eeac8d3d06dcee65ac76
crispycodeandchips <bb9e.bb8.c3po.r2d2@gmail.com>
2021-07-08 14:22:00 +0900

    충돌 테스트!!!

--- src/gitHello/Hello.java
+++ src/gitHello/Hello.java
@@ -3,9 +3,9 @@ package gitHello;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("hello");
-		
+		System.out.println("월요일부터는 비대면 수업입니다.");
 		System.out.println("안녕하세요 홍길동 님");
-		System.out.println("만나서 반갑습니다.");
+		System.out.println("짜장면을 먹었더니 졸음이 옵니다");
 		System.out.println("오늘은 목요일 입니다.");
 	}
 
